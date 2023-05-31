a99da0a5a00bc308370e77b198e46e9a5e5e27ad
Daniel Gonzalez <dgonzalezsan85@gmail.com>
2023-05-31 18:11:24 -0500

    Ejercicio 3 terminado

--- EjerciciosJavaIntro/src/main/java/com/danielgonzalez/ejerciciosjavaintro/Ejercicio03JavaIntro.java
+++ EjerciciosJavaIntro/src/main/java/com/danielgonzalez/ejerciciosjavaintro/Ejercicio03JavaIntro.java
@@ -4,17 +4,32 @@
  */
 package com.danielgonzalez.ejerciciosjavaintro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author danielgonzalez
  */
+/*
+Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda
+en minúsculas. Nota: investigar la función toUpperCase() y toLowerCase() en Java.
+*/
 public class Ejercicio03JavaIntro {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingresa una frase");
+        String frase = leer.nextLine();
+        
+        System.out.println(frase.toUpperCase());  
+        
+        System.out.println(frase.toLowerCase());  
+
     }
     
 }
