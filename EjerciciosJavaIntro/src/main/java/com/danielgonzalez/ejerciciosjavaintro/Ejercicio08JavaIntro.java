e678cf7ada142aa210157f0fb2383c5e2fc852f4
Daniel Gonzalez <dgonzalezsan85@gmail.com>
2023-05-31 19:04:16 -0500

    Ejercicio 8

---
+++ EjerciciosJavaIntro/src/main/java/com/danielgonzalez/ejerciciosjavaintro/Ejercicio08JavaIntro.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.danielgonzalez.ejerciciosjavaintro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danielgonzalez
+ */
+/*
+Realizar un programa que solo permita introducir solo frases o palabras de 8 de largo. Si el
+usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje por
+pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”.
+Nota: investigar la función Lenght() en Java.
+*/
+public class Ejercicio08JavaIntro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingresaremos una frase");
+
+        String frase = leer.nextLine();
+        
+        System.out.println("");
+        tamanoFrase(frase);
+    }
+    
+    public static void tamanoFrase(String frase){
+        
+        if (frase.length() == 8) {
+            System.out.println("CORRECTO");
+        } else {
+            System.out.println("INCORRECTO");
+        }
+    }
+}
